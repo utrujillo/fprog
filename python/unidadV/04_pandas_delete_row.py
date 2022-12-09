61946203c62cedd953a8fd1756220729ed9c60c3
Uziel Trujillo <uziel.trujillo.mx@cloudappi.net>
2022-12-09 08:32:19 -0600

    Eliminar una fila 	modified:   python/unidadV/04_pandas_delete_row.py

--- python/unidadV/04_pandas_delete_row.py
+++ python/unidadV/04_pandas_delete_row.py
@@ -3,5 +3,5 @@ import pandas as pd
 # Create DataFrame
 dataFrame = pd.DataFrame([[10, 15], [20, 25], [30, 35], [40, 45]],index=['w', 'x', 'y', 'z'],columns=['a', 'b'])
 print( dataFrame )
-dataFrame = dataFrame.drop('y')
+dataFrame = dataFrame.drop('z')
 print( dataFrame )
